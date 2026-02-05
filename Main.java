3d970034da34962f2e67875308ea9f17d6ce6827
danielapolanco <pinondaniela86@gmail.com>
2026-02-05 09:17:18 -0700

    lsita de integrantes

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("INTEGRANTES DEL EQUIPO");
+        System.out.println("Daniela Piñon Polanco");
+        System.out.println("Poleth Elizama Escarcega Lopez");
+        System.out.println("Georgina");
+    }
+}
